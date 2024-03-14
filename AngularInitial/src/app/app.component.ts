69acafddde4ceb9fdc2a7a3df75e5810f79dff09
Yjoubayr <yjoubayr972@boscdelacoma.cat>
2024-03-14 16:57:02 +0100

    Vista del turarial feta, falta logica

--- AngularInitial/src/app/app.component.ts
+++ AngularInitial/src/app/app.component.ts
@@ -1,13 +1,29 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { HomeComponent } from './home/home.component';
+import { HousingLocationComponent } from './housing-location/housing-location.component';
+
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
-  templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+  imports: [RouterOutlet, HomeComponent, HousingLocationComponent],
+  //templateUrl: '../index.html',
+  template: `
+    <main>
+    <header class="brand-name">
+      <img class="brand-logo" src="favicon.ico" alt="logo" aria-hidden="true">
+    </header>
+    <section class="content">
+      <app-home></app-home>
+    </section>
+  </main>
+  `,
+  styleUrl: './app.component.css',
+  
 })
 export class AppComponent {
-  title = 'AngularInitial';
+  // title = 'AngularInitial';
+  title = 'homes';
+
 }
