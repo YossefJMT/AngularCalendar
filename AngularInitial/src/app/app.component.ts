5fc5fab8e01f4aa110cd3cbb32b96761d1b42a56
Yjoubayr <yjoubayr972@boscdelacoma.cat>
2024-03-15 11:19:46 +0100

    Tutorial acabar y creada estructura del projecte calendari

--- AngularInitial/src/app/app.component.ts
+++ AngularInitial/src/app/app.component.ts
@@ -1,29 +1,28 @@
 import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
 import { HomeComponent } from './home/home.component';
-import { HousingLocationComponent } from './housing-location/housing-location.component';
-
+import { RouterModule } from '@angular/router';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, HomeComponent, HousingLocationComponent],
-  //templateUrl: '../index.html',
+  imports: [
+    HomeComponent,
+    RouterModule,
+  ],
   template: `
     <main>
-    <header class="brand-name">
-      <img class="brand-logo" src="favicon.ico" alt="logo" aria-hidden="true">
-    </header>
-    <section class="content">
-      <app-home></app-home>
-    </section>
-  </main>
+      <a [routerLink]="['/']">
+        <header class="brand-name">
+          <img class="brand-logo" src="/assets/logo.svg" alt="logo" aria-hidden="true">
+        </header>
+      </a>
+      <section class="content">
+        <router-outlet></router-outlet>
+      </section>
+    </main>
   `,
-  styleUrl: './app.component.css',
-  
+  styleUrls: ['./app.component.css'],
 })
 export class AppComponent {
-  // title = 'AngularInitial';
   title = 'homes';
-
 }
--- AngularInitial/src/app/home/home.component.ts
+++ AngularInitial/src/app/home/home.component.ts
@@ -1,4 +1,6 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { HousingService } from '../housing.service';
 import { HousingLocation } from '../housinglocation';
 import { HousingLocationComponent } from "../housing-location/housing-location.component";
 
@@ -8,30 +10,37 @@ import { HousingLocationComponent } from "../housing-location/housing-location.c
     template: `
     <section>
       <form>
-        <h1 class="title">Home</h1>
-        <input type="text" placeholder="Filter by city">
-        <button class="primary" type="button">Search</button>
+        <h1>Home</h1>
+        <input type="text" placeholder="Filter by city" #filter>
+        <button class="primary" type="button" (click)="filterResults(filter.value)">Search</button>
       </form>
     </section>
     <section class="results">
-      <app-housing-location [housingLocation]="housingLocation"></app-housing-location>
-    </section>
+      <app-housing-location *ngFor="let housingLocation of filteredLocationList" [housingLocation]="housingLocation"></app-housing-location>
+  </section>
   `,
     styleUrl: './home.component.css',
-    imports: [HousingLocationComponent]
+    imports: [HousingLocationComponent, CommonModule]
 })
 
 export class HomeComponent {
-  readonly baseUrl = 'https://angular.io/assets/images/tutorials/faa';
+  housingLocationList: HousingLocation[] = [];
+  housingService: HousingService = inject(HousingService);
+  filteredLocationList: HousingLocation[] = [];
 
-  housingLocation: HousingLocation = {
-    id: 9999,
-    name: 'Test Home',
-    city: 'Test city',
-    state: 'ST',
-    photo: `${this.baseUrl}/example-house.jpg`,
-    availableUnits: 99,
-    wifi: true,
-    laundry: false,
-  };
+  constructor() {
+    this.housingLocationList = this.housingService.getAllHousingLocations();
+    this.filteredLocationList = this.housingLocationList;
+  }
+
+  filterResults(text: string) {
+    if (!text) {
+      this.filteredLocationList = this.housingLocationList;
+      return;
+    }
+  
+    this.filteredLocationList = this.housingLocationList.filter(
+      housingLocation => housingLocation?.city.toLowerCase().includes(text.toLowerCase())
+    );
+  }
 }
