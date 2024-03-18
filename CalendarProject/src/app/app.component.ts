0bc9a4cbaf8c18f9db5242f34cfba45d23a89fac
Yjoubayr <yjoubayr972@boscdelacoma.cat>
2024-03-18 09:15:44 +0100

    Year view and styling

--- CalendarProject/src/app/app.component.ts
+++ CalendarProject/src/app/app.component.ts
@@ -1,13 +1,42 @@
-import { Component } from '@angular/core';
+import { Component, CUSTOM_ELEMENTS_SCHEMA } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { CalendarOptions } from '@fullcalendar/core'; // useful for typechecking
+import { FullCalendarModule } from '@fullcalendar/angular'; // import FullCalendarModule
+import multiMonthPlugin from '@fullcalendar/multimonth'; // multi-month plugin
+
+import dayGridPlugin from '@fullcalendar/daygrid';
+import interactionPlugin, { DateClickArg } from '@fullcalendar/interaction';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
-  templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+  imports: [RouterOutlet, FullCalendarModule], // add FullCalendarModule to imports
+  // templateUrl: './app.component.html',
+  template: '<full-calendar class="calendar" [options]="calendarOptions"></full-calendar>',
+  styleUrl: './app.component.css',
+  schemas: [CUSTOM_ELEMENTS_SCHEMA] // add CUSTOM_ELEMENTS_SCHEMA
 })
+
 export class AppComponent {
   title = 'CalendarProject';
+  calendarOptions: CalendarOptions = {
+    plugins: [dayGridPlugin, interactionPlugin, multiMonthPlugin],
+    initialView: 'multiMonthYear',
+    views: {
+      multiMonthFourMonth: {
+        type: 'multiMonth',
+        duration: { months: 4 }
+      }
+    },
+
+    dateClick: (arg) => this.handleDateClick(arg),
+    events: [
+      { title: 'event 1', date: '2024-03-20' },
+      { title: 'event 2', date: '2024-03-24' }
+    ]
+  };
+
+  handleDateClick(arg: DateClickArg) {
+    alert('date click! ' + arg.dateStr)
+  }
 }
---
+++ CalendarProject/src/app/app.module.ts
@@ -0,0 +1,20 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+import { FullCalendarModule } from '@fullcalendar/angular';
+import { AppComponent } from './app.component';
+
+@NgModule({
+    declarations: [
+        // AppComponent
+    ],
+    imports: [
+        BrowserModule,
+        FullCalendarModule,
+        AppComponent
+    ],
+    providers: [],
+    bootstrap: [
+        // AppComponent
+    ]
+})
+export class AppModule { }
