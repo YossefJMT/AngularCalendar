c5197c99b179cbae9857c2b31db5668149e3af9d
Yjoubayr <YossefJMT@gmail.com>
2024-03-19 09:12:04 +0100

    styling

--- CalendarProject/src/app/app.component.ts
+++ CalendarProject/src/app/app.component.ts
@@ -1,42 +1,76 @@
 import { Component, CUSTOM_ELEMENTS_SCHEMA } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
-import { CalendarOptions } from '@fullcalendar/core'; // useful for typechecking
-import { FullCalendarModule } from '@fullcalendar/angular'; // import FullCalendarModule
-import multiMonthPlugin from '@fullcalendar/multimonth'; // multi-month plugin
-
+import { Calendar, CalendarOptions, DateSelectArg } from '@fullcalendar/core';
+import { FullCalendarModule } from '@fullcalendar/angular';
+import multiMonthPlugin from '@fullcalendar/multimonth';
 import dayGridPlugin from '@fullcalendar/daygrid';
-import interactionPlugin, { DateClickArg } from '@fullcalendar/interaction';
+import interactionPlugin, { DateClickArg } from '@fullcalendar/interaction'; // Importa DateClickArg aquí
+import eventos from '../data/events';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, FullCalendarModule], // add FullCalendarModule to imports
-  // templateUrl: './app.component.html',
-  template: '<full-calendar class="calendar" [options]="calendarOptions"></full-calendar>',
-  styleUrl: './app.component.css',
-  schemas: [CUSTOM_ELEMENTS_SCHEMA] // add CUSTOM_ELEMENTS_SCHEMA
+  imports: [FullCalendarModule],
+  template: '<div class="calendar" id="calendar"></div>',
+  styleUrls: ['./app.component.css'],
+  schemas: [CUSTOM_ELEMENTS_SCHEMA]
 })
-
 export class AppComponent {
   title = 'CalendarProject';
-  calendarOptions: CalendarOptions = {
-    plugins: [dayGridPlugin, interactionPlugin, multiMonthPlugin],
-    initialView: 'multiMonthYear',
-    views: {
-      multiMonthFourMonth: {
-        type: 'multiMonth',
-        duration: { months: 4 }
+  calendar!: Calendar;
+
+  ngAfterViewInit() {
+    this.initializeCalendar();
+  }
+
+  initializeCalendar() {
+    if (typeof document !== 'undefined') {
+      const calendarEl = document.getElementById('calendar');
+      if (calendarEl) {
+        this.calendar = new Calendar(calendarEl, this.getCalendarOptions());
+        this.calendar.render();
       }
-    },
+    }
+  }
 
-    dateClick: (arg) => this.handleDateClick(arg),
-    events: [
-      { title: 'event 1', date: '2024-03-20' },
-      { title: 'event 2', date: '2024-03-24' }
-    ]
-  };
+  getCalendarOptions(): CalendarOptions {
+    return {
+      plugins: [dayGridPlugin, interactionPlugin, multiMonthPlugin],
+      initialView: 'multiMonthYear',
+      selectable: true,
+      select: this.onSelect.bind(this),
+      //dateClick: this.onDateClick.bind(this),
+      events: [
+        ...eventos.FestiuEstatal,
+        ...eventos.FestiuLocal,
+        ...eventos.PontsAltres,
+        ...eventos.Personal,
+        ...eventos.PermisosBaixes,
+        ...eventos.PendentConfirmacio,
+        ...eventos.NoAcceptades
+      ]
+    };
+  }
 
-  handleDateClick(arg: DateClickArg) {
-    alert('date click! ' + arg.dateStr)
+  onSelect(arg: DateSelectArg) {
+    const result = window.confirm(`¿Qué acción deseas realizar? ${arg.start} - ${arg.end}`);
+    if (result) {
+      this.makeEstatalEvent(arg.start, arg.end);
+    }
+  }
+
+  onDateClick(arg: DateClickArg) {
+    const result = window.confirm('¿Qué acción deseas realizar?');
+    if (result) {
+      this.makeEstatalEvent(arg.date, arg.date);
+    }
+  }
+
+  makeEstatalEvent(start: Date, end: Date) {
+    // Implementar la lógica para crear un evento estatal utilizando las fechas de inicio y fin
+    const event = { title: 'Estatal Event', start, end, backgroundColor: 'blue' };
+
+    // Agregar el evento al calendario
+    this.calendar.addEvent(event);
+    alert('Evento creado');
   }
 }
---
+++ CalendarProject/src/data/events.ts
@@ -0,0 +1,27 @@
+const eventos = {
+    FestiuEstatal: [
+      { title: 'Festiu Estatal', date: '2024-03-20', backgroundColor: 'red'},
+    ],
+    FestiuLocal: [
+      { title: 'Festiu Local', date: '2024-03-21', backgroundColor: 'orange'},
+    ],
+    PontsAltres: [
+      { title: 'Ponts/Altres', date: '2024-03-22', backgroundColor: 'blue'},
+    ],
+    Personal: [
+      { title: 'Vacances', date: '2024-03-23', backgroundColor: 'green' },
+      { title: 'Assumptes Propis', date: '2024-03-24', backgroundColor: 'pink' },
+    ],
+    PermisosBaixes: [
+      { title: 'Permís/Baixa', date: '2024-03-26', backgroundColor: 'aquamarine' },
+    ],
+    PendentConfirmacio: [
+      { title: 'Pendent confirmació', date: '2024-03-27', backgroundColor: 'yellow' },
+    ],
+    NoAcceptades: [
+      { title: 'No acceptades', date: '2024-03-28', backgroundColor: 'purple' },
+    ]
+  };
+  
+export default eventos;
+  
