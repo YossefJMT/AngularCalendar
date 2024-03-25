b736df10e2e52b04a81ce649ea3499e8656435ed
Yjoubayr <YossefJMT@gmail.com>
2024-03-25 12:49:50 +0100

    afegida logica de calendari escolar, falta millorar el codi i fer detalls

--- CalendarProject/src/app/app.component.ts
+++ CalendarProject/src/app/app.component.ts
@@ -25,14 +25,13 @@ export class AppComponent {
     private dialog: MatDialog,
     private viewContainerRef: ViewContainerRef
   ) {}
-
-  
   title = 'CalendarProject';
   calendar!: Calendar;
 
   ngAfterViewInit() {
     this.initializeCalendar();
-    this.makeSchoolYear();
+    // this.makeSchoolYear();
+    this.addEventListeners();
   }
 
   initializeCalendar() {
@@ -51,7 +50,8 @@ export class AppComponent {
     return {
       plugins: [dayGridPlugin, interactionPlugin, multiMonthPlugin],
       initialView: 'multiMonthYear',
-      multiMonthMaxColumns: 1,
+      // initialDate: '2022-03-03',
+      monthStartFormat: { month: 'long', year: 'numeric', day: 'numeric'},
       selectable: true,
       select: this.onSelect.bind(this),
       events: [
@@ -71,11 +71,19 @@ export class AppComponent {
         cancelBtn: {
           text: 'cancel',
           click: this.removeSelectedClass
+        },
+        inicioAñoEscolar: {
+          text: 'School Year',
+          click: (ev: MouseEvent, element: HTMLElement) => this.showInitialSchoolYear(this.calendar)
+        },
+        finalAñoEscolar: {
+          text: 'School Year',
+          click: (ev: MouseEvent, element: HTMLElement) => this.showFinalSchoolYear(this.calendar)
         }
       },
       headerToolbar: {
         left: 'prev,today,next confirmBtn',
-        center: 'title',
+        center: 'inicioAñoEscolar title finalAñoEscolar',
         right: 'cancelBtn multiMonthYear,dayGridMonth'
       },
       eventClick: function(info){
@@ -158,6 +166,9 @@ export class AppComponent {
 
   confirmFunction = async () => {
     const datesSelected = document.querySelectorAll('.selected');
+    if (datesSelected.length == 0) {
+      return;
+    }
     const eventsType = await this.askEventType();
     const color = this.getColor(eventsType);
 
@@ -199,15 +210,116 @@ export class AppComponent {
     });
   }
 
-  makeSchoolYear() {
-    // const dates = ['2024-07', '2024-08', '2024-10']
-    // dates.forEach(month => {
-    //   const dates = document.querySelectorAll(`[data-date^="${month}"]`);
-    //   dates.forEach(div => {
-    //     div.classList.add('hidden');
-    //   });
-    // });
-    
+  makeShowInitialSchoolYear(year: number) {
+    const dates = [year+'-01', year+'-02', year+'-03', year+'-04', year+'-05', year+'-06', year+'-07', year+'-08'];
+    dates.forEach(month => {
+      const dates = document.querySelectorAll(`[data-date^="${month}"]`);
+      dates.forEach(div => {
+        div.classList.add('hidden');
+      });
+    });
 
+    // mostrar las demas
+    const datesToShow = [year+'-09', year+'-10', year+'-11', year+'-12'];
+    datesToShow.forEach(month => {
+      const dates = document.querySelectorAll(`[data-date^="${month}"]`);
+      dates.forEach(div => {
+        div.classList.remove('hidden');
+      });
+    });
+
+    const elementInicioview = document.querySelector('.fc-inicioAñoEscolar-button');
+    if (elementInicioview) {
+      elementInicioview.textContent = year.toString();
+    }
+    
+    const elementFinalview = document.querySelector('.fc-finalAñoEscolar-button');
+    if (elementFinalview) {
+      elementFinalview.textContent = (year+1).toString();
+    }
+  }
+
+  makeShowFinalSchoolYear(year: number) {
+    const dates = [year+'-09', year+'-10', year+'-11', year+'-12', year+'-07', year+'-08'];
+    dates.forEach(month => {
+      const dates = document.querySelectorAll(`[data-date^="${month}"]`);
+      dates.forEach(div => {
+        div.classList.add('hidden');
+      });
+    });
+    
+    // mostrar las demas
+    const datesToShow = [year+'-01', year+'-02', year+'-03', year+'-04', year+'-05', year+'-06'];;
+    datesToShow.forEach(month => {
+      const dates = document.querySelectorAll(`[data-date^="${month}"]`);
+      dates.forEach(div => {
+        div.classList.remove('hidden');
+      });
+    });
+
+    const elementInicioview = document.querySelector('.fc-inicioAñoEscolar-button');
+    if (elementInicioview) {
+      // quitamos el atributo title
+      elementInicioview.removeAttribute('title');
+      elementInicioview.innerHTML = (year-1).toString();
+    }
+
+    const elementFinalview = document.querySelector('.fc-finalAñoEscolar-button');
+    if (elementFinalview) {
+      elementFinalview.innerHTML = year.toString();
+    }
+  }
+
+  addEventListeners() {
+
+    const elementInicioview = document.querySelector('.fc-inicioAñoEscolar-button');
+    const elementFinalview = document.querySelector('.fc-finalAñoEscolar-button');
+
+    const prevButton = document.querySelector('.fc-prev-button');
+    const nextButton = document.querySelector('.fc-next-button');
+
+    prevButton?.addEventListener('click', () => {
+      const date = this.calendar.getCurrentData().dateProfile.currentRange.start.toISOString().slice(0, 10);
+      // window.alert('Mostrando el inicio del año escolar: ' + date);
+      const yearDate = date.split('-')[0];
+      window.alert('Mostrando el inicio del año escolar');
+      if (elementFinalview) {
+        elementFinalview.innerHTML = yearDate;
+      }
+      if (elementInicioview) {
+        elementInicioview.innerHTML = yearDate;
+      }
+    });
+
+    nextButton?.addEventListener('click', () => {
+      const date = this.calendar.getCurrentData().dateProfile.currentRange.start.toISOString().slice(0, 10);
+      // window.alert('Mostrando el inicio del año escolar: ' + date);
+      const yearDate = date.split('-')[0];
+      window.alert('Mostrando el final del año escolar');
+      if (elementFinalview) {
+        elementFinalview.innerHTML = yearDate;
+      }
+      if (elementInicioview) {
+        elementInicioview.innerHTML = yearDate;
+      }
+    });
+  }
+
+  showInitialSchoolYear(Calendar: Calendar) {
+    // window.alert('Mostrando el inicio del año escolar');
+    const date = Calendar.getCurrentData().dateProfile.currentRange.start.toISOString().slice(0, 10);
+    // window.alert('Mostrando el inicio del año escolar: ' + date);
+    const yearDate = date.split('-')[0];
+    Calendar.prevYear();
+    this.makeShowInitialSchoolYear(parseInt(yearDate)-1);
+  }
+
+  showFinalSchoolYear(Calendar: Calendar) {
+    // window.alert('Mostrando el inicio del año escolar');
+    const date = Calendar.getCurrentData().dateProfile.currentRange.start.toISOString().slice(0, 10);
+    // window.alert('Mostrando el inicio del año escolar: ' + date);
+    const yearDate = date.split('-')[0];
+    Calendar.nextYear();
+    this.makeShowFinalSchoolYear(parseInt(yearDate)+1);
   }
 }
